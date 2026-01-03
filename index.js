e8489e821d50392edc0d2c4f95198fa944beab1c
YusufKosarDev <yusufkosare004@gmail.com>
2026-01-03 16:28:05 +0300

    gorev tamamlandi

--- index.js
+++ index.js
@@ -50,8 +50,8 @@ function KareninAlani(kenaruzunlugu) {
 	4. Hesaplanan çemberin çevresi döndürülecektir.
 */
 
-function CemberinCevresi(/* kodlar buraya */) {
-  /* kodlar buraya */
+function CemberinCevresi(yaricap) {
+      return 2 * pi * yaricap;
 }
 
 /* (Oto test yok) Yukarıdaki CemberinCevresi fonksiyonunu yarıçap = 5 vererek aşağıda çalıştırıp, sonucu konsolda gözlemleyin (console.log)  */
@@ -64,8 +64,8 @@ function CemberinCevresi(/* kodlar buraya */) {
 	4. Hesaplanan çemberin alanı döndürülecektir.
 */
 
-function CemberinAlani(/* kodlar buraya */) {
-  /* kodlar buraya */
+function CemberinAlani(yaricap, pi) {
+    return pi * Math.pow(yaricap, 2);
 }
 
 /* (Oto test yok) Yukarıdaki CemberinAlani fonksiyonunu yarıçap = 15 vererek aşağıda çalıştırıp, sonucu konsolda gözlemleyin (console.log)  */
@@ -98,28 +98,67 @@ let ucetambolunenler,
   tekraredensayilar;
 
 // 3a çözümü
+enkucuk = sayilar[0];
+enbuyuk = sayilar[0];
 
-/* kodlar buraya */
+for (let i = 1; i < sayilar.length; i++) {
+  if (sayilar[i] < enkucuk) {
+    enkucuk = sayilar[i];
+  }
+  if (sayilar[i] > enbuyuk) {
+    enbuyuk = sayilar[i];
+  }
+}
+
 
 // 3b çözümü:
+ucetambolunenler = [];
 
-/* kodlar buraya */
+sayilar.forEach((sayi) => {
+  if (sayi % 3 === 0) {
+    ucetambolunenler.push(sayi);
+  }
+});
+
 
 // 3c çözümü:
+ucebolunenlerintoplami = ucetambolunenler.reduce(
+  (toplam, sayi) => toplam + sayi,
+  0
+);
 
-/* kodlar buraya */
 
 // 3d çözümü
+besyuzdenkucuksayilar = sayilar.filter((sayi) => sayi < 500);
 
-/* kodlar buraya */
 
 // 3e çözümü
+siralisayilar = [...besyuzdenkucuksayilar].sort((a, b) => a - b);
 
-/* kodlar buraya */
 
 // 3f çözümü
+tekraredensayilar = [];
 
-/* kodlar buraya */
+const sayac = {};
+
+for (let i = 0; i < sayilar.length; i++) {
+  const sayi = sayilar[i];
+  if (sayac[sayi]) {
+    sayac[sayi]++;
+  } else {
+    sayac[sayi] = 1;
+  }
+}
+for (let key in sayac) {
+  if (sayac[key] > 1) {
+    tekraredensayilar.push(
+      `${key} sayısı ${sayac[key]} kere tekrar edilmiştir`
+    );
+  }
+}
+console.log(sayilar.length);
+
+
 
 /*  Bu satırın aşağısındaki kodları lütfen değiştirmeyin  */
 
